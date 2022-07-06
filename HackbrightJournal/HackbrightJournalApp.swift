1d33d07ad0f13dcb5e015997769a7dc33887b2fc
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-06 09:43:42 -0700

    Initial project

---
+++ HackbrightJournal/HackbrightJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackbrightJournalApp.swift
+//  HackbrightJournal
+//
+//  Created by Yuliya  on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct HackbrightJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
